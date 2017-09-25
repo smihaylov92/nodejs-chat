ea6be8370cb8dc0aacee50f99730c62803b92101
Stanislav Mihaylov <smihaylov@questarai.com>
2017-09-25 15:16:35 +0300

    Fix redux, change styles

--- client/utils.js
+++ client/utils.js
@@ -3,7 +3,7 @@ var Utils = {
 		var today = new Date(),
 			date = this.prefixDatePart(today.getDate()) + '-' + this.prefixDatePart((today.getMonth() + 1)) + '-' + today.getFullYear(),
 			time = this.prefixDatePart(today.getHours()) + ':' + this.prefixDatePart(today.getMinutes()) + ':' + this.prefixDatePart(today.getSeconds()),
-			timestamp = date + ' ' + time;
+			timestamp = time + ' ' + date;
 
 		return timestamp;
 	},
